80496273b8c966473309e1fe96af2c6571bda7c3
Meteor <v138468@gmail.com>
2022-09-03 02:06:32 +0800

    生成库

---
+++ lib_source_code/add.cpp
@@ -0,0 +1,18 @@
+/**
+ * @file add.cpp
+ * @author v138468 (v138468@gmail.com)
+ * @brief 生成静态库与动态库用
+ * @version 0.1
+ * @date 2022-09-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#include "add.h"
+
+int add(int a, int b)
+{
+    std::cout << "a + b = " << a + b << std::endl;
+    return a + b;
+}
---
+++ lib_source_code/add.h
@@ -0,0 +1,15 @@
+/**
+ * @file add.h
+ * @author v138468 (v138468@gmail.com)
+ * @brief 生成静态库与动态库用
+ * @version 0.1
+ * @date 2022-09-03
+ *
+ * @copyright Copyright (c) 2022
+ *
+ */
+
+#pragma once
+#include <iostream>
+
+int add(int a, int b);
